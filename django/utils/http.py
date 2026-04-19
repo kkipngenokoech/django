acb0fad803fa1709219a8e3dced7b77c69cc1d10
kipngeno koech <kkipngenokoech22@gmail.com>
2026-04-19 17:18:47 +0200

    fix: handle empty string for If-Modified-Since header gracefully

--- django/utils/http.py
+++ django/utils/http.py
@@ -146,6 +146,8 @@ def parse_http_date_safe(date):
     """
     Same as parse_http_date, but return None if the input is invalid.
     """
+    if not date:
+        return None
     try:
         return parse_http_date(date)
     except Exception:
